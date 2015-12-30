93539ba6893862b9398753c339d83a390305d8ba
mfrancis95 <mikefrancis95@gmail.com>
2015-12-30 18:59:47 -0500

    Added an optional argument for not equal.

--- on.py
+++ on.py
@@ -3,12 +3,22 @@ import sys
 from subprocess import call
 
 parser = ArgumentParser("on")
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-eq", action = "store_true")
+group.add_argument("-ne", action = "store_true")
+
 parser.add_argument("value")
 parser.add_argument("command", nargs = "+")
 args = parser.parse_args()
 
 value = args.value
+if args.ne:
+    compare = lambda line: line != value
+else:
+    compare = lambda line: line == value
 process = " ".join(args.command)
+
 for line in sys.stdin:
-    if line.rstrip() == value:
+    if compare(line.rstrip()):
         call(process, shell = True)
