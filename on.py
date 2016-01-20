5127d828e1d7f71c52f49b10be4c7941d02020cf
mfrancis95 <mikefrancis95@gmail.com>
2016-01-20 07:45:45 -0500

    Add several comparison operators

--- on.py
+++ on.py
@@ -4,26 +4,32 @@ import subprocess
 
 parser = ArgumentParser("on")
 parser.add_argument("-a", "--async", action = "store_true")
-
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-eq", action = "store_true")
-group.add_argument("-ne", action = "store_true")
+parser.add_argument("operator", choices = ["eq", "ne", "gt", "lt", "gte", "lte"])
 
 parser.add_argument("value")
 parser.add_argument("command", nargs = "+")
 parser.add_argument("-q", action = "store_true")
 args = parser.parse_args()
 
+operators = {
+    "eq": lambda line: line.strip() == value,
+    "ne": lambda line: line.strip() != value,
+    "gt": lambda line: int(line) > value,
+    "lt": lambda line: int(line) < value,
+    "gte": lambda line: int(line) >= value,
+    "lte": lambda line: int(line) <= value
+}
+
 call = subprocess.Popen if args.async else subprocess.call
 value = args.value.strip()
-if args.ne:
-    compare = lambda line: line.strip() != value
-else:
-    compare = lambda line: line.strip() == value
+operator = args.operator
+if operator not in ("eq", "ne"):
+    value = int(value)
+operator = operators[operator]
 process = " ".join(args.command)
 quit = args.q
 
-for line in filter(compare, sys.stdin):
+for line in filter(operator, sys.stdin):
     call(process, shell = True)
     if quit:
         break
