dbfe21270cab9e1c924186508b31f60942f80794
mfrancis95 <mikefrancis95@gmail.com>
2016-01-25 14:41:04 -0500

    Code improvements

--- on.py
+++ on.py
@@ -2,15 +2,6 @@ from argparse import ArgumentParser
 import sys
 import subprocess
 
-parser = ArgumentParser("on")
-parser.add_argument("-a", "--async", action = "store_true")
-parser.add_argument("operator", choices = ["eq", "ne", "gt", "lt", "gte", "lte"])
-
-parser.add_argument("value")
-parser.add_argument("command", nargs = "+")
-parser.add_argument("-q", action = "store_true")
-args = parser.parse_args()
-
 operators = {
     "eq": lambda line: line.strip() == value,
     "ne": lambda line: line.strip() != value,
@@ -19,6 +10,14 @@ operators = {
     "gte": lambda line: int(line) >= value,
     "lte": lambda line: int(line) <= value
 }
+
+parser = ArgumentParser("on")
+parser.add_argument("-a", "--async", action = "store_true")
+parser.add_argument("operator", choices = operators.keys())
+parser.add_argument("value")
+parser.add_argument("command", nargs = "+")
+parser.add_argument("-q", action = "store_true")
+args = parser.parse_args()
 
 call = subprocess.Popen if args.async else subprocess.call
 value = args.value.strip()
