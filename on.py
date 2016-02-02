a8c25fce7d375795b511589d80d6e7879fab82d2
mfrancis95 <mikefrancis95@gmail.com>
2016-02-01 23:14:25 -0500

    Add --not argument for negating the comparison operator

--- on.py
+++ on.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
-import sys
 import subprocess
+from itertools import filterfalse
+import sys
 
 operators = {
     "eq": lambda line: line.strip() == value,
@@ -13,20 +14,22 @@ operators = {
 
 parser = ArgumentParser("on")
 parser.add_argument("-a", "--async", action = "store_true")
+parser.add_argument("-n", "--not", action = "store_true")
 parser.add_argument("operator", choices = operators.keys())
 parser.add_argument("value")
 parser.add_argument("command", nargs = "+")
-parser.add_argument("-q", action = "store_true")
-args = parser.parse_args()
+parser.add_argument("-q", "--quit", action = "store_true")
+args = vars(parser.parse_args())
 
-call = subprocess.Popen if args.async else subprocess.call
-value = args.value.strip()
-operator = args.operator
+call = subprocess.Popen if args["async"] else subprocess.call
+filter = filterfalse if args["not"] else filter
+value = args["value"].strip()
+operator = args["operator"]
 if operator not in ("eq", "ne"):
     value = int(value)
 operator = operators[operator]
-process = " ".join(args.command)
-quit = args.q
+process = " ".join(args["command"])
+quit = args["quit"]
 
 for line in filter(operator, sys.stdin):
     call(process, shell = True)
