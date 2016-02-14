db8ed86efe53cda95710631f43da680aaaa7d4d8
mfrancis95 <mikefrancis95@gmail.com>
2016-02-14 17:16:49 -0500

    Convert lines to floats instead of ints

--- on.py
+++ on.py
@@ -1,17 +1,17 @@
-from argparse import ArgumentParser
 from re import match
+from argparse import ArgumentParser
 import subprocess
 from itertools import filterfalse
 import sys
 
 operators = {
-    "eq": lambda line: line.strip() == value,
-    "ne": lambda line: line.strip() != value,
-    "like": lambda line: match(value, line.strip()),
-    "gt": lambda line: int(line) > value,
-    "lt": lambda line: int(line) < value,
-    "gte": lambda line: int(line) >= value,
-    "lte": lambda line: int(line) <= value
+    "eq": lambda line: line == value,
+    "ne": lambda line: line != value,
+    "like": lambda line: match(value, line),
+    "gt": lambda line: float(line) > value,
+    "lt": lambda line: float(line) < value,
+    "gte": lambda line: float(line) >= value,
+    "lte": lambda line: float(line) <= value
 }
 
 parser = ArgumentParser("on")
@@ -28,12 +28,12 @@ filter = filterfalse if args["not"] else filter
 value = args["value"].strip()
 operator = args["operator"]
 if operator not in ("eq", "ne", "like"):
-    value = int(value)
+    value = float(value)
 operator = operators[operator]
 process = " ".join(args["command"])
 quit = args["quit"]
 
-for line in filter(operator, sys.stdin):
+for line in filter(operator, map(lambda line: line.strip(), sys.stdin)):
     call(process, shell = True)
     if quit:
         break
